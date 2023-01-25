afb3fdeee63d8fb1b62bdc6f965fab2b6caf06c1
Joshua Perry <45966243+jpez-development@users.noreply.github.com>
2023-01-25 15:37:46 +0000

    updated portal template

--- templates/portal.html.php
+++ templates/portal.html.php
@@ -1,12 +1,11 @@
 <main class="sidebar">
 	<section class="left">
 		<ul>
-			<li><a href="jobs.php">Jobs</a></li>
-			<li><a href="categories.php">Categories</a></li>
+			<li><a href="/portal">Jobs</a></li>
+			<li><a href="/portal/categories">Categories</a></li>
 		</ul>
 	</section>
-
 	<section class="right">
-	    <?=$table?>
+	    <?=require $table;?>
     </section>
 </main>
