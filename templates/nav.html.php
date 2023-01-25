5976f5722a85e0e7d2891c76e282bfd19d222f40
Joshua Perry <45966243+jpez-development@users.noreply.github.com>
2023-01-25 13:42:11 +0000

    added login button

--- templates/nav.html.php
+++ templates/nav.html.php
@@ -1,5 +1,22 @@
-<?php foreach($cats as $cat) { ?>
-    <li>
-        <a href="/jobs/category?page=<?=urlencode($cat->name)?>"><?=$cat->name?></a>
+<ul>
+    <li><a href="/">Home</a></li>
+    <li>Jobs
+        <ul>
+            <?php foreach($cats as $cat) { ?>
+                <li>
+                    <a href="/jobs/category?page=<?=urlencode($cat->name)?>"><?=$cat->name?></a>
+                </li>
+            <?php } ?>    
+        </ul>
     </li>
-<?php } ?>
+    <li><a href="/jobs/faq">FAQ</a></li>
+    <li><a href="/jobs/about">About Us</a></li>
+    <?php if (isset($_SESSION['loggedin'])) {?>
+        <li><a href="/user/logout">Logout</a></li>
+    <?php } 
+    else {?>
+        <li><a href="/user/login">Login</a></li>
+    <?php } ?>
+</ul>
+
+
